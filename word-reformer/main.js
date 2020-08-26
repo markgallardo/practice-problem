1f149278f2e9fe4bac3278298cc33b10a68ec1cb
Mark Gallardo <markgallardo1221@gmail.com>
2020-08-26 10:29:45 -0700

    word-reformer

---
+++ word-reformer/main.js
@@ -0,0 +1,6 @@
+function wordReformer(wordfragment1,wordfragment2){
+  return wordfragment1+wordfragment2;
+}
+console.log(wordReformer("bro","ken"));
+console.log(wordReformer("manipu","lation"));
+console.log(wordReformer("excite","ment"))
